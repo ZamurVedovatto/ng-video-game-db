729eefced3449d86b3574eabe613f18658195acf
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-03-17 21:27:10 -0300

    interceptors (errors and headers)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NgVideoGameDb
 
 https://www.youtube.com/watch?v=LiOzTQAz13Q
+
+
+## TODO
+register on rapidApi: https://rapidapi.com/accujazz/api/rawg-video-games-database/details
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
+import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -34,7 +37,18 @@ import { HomeComponent } from './components/home/home.component';
     MatTabsModule,
     MatIconModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
