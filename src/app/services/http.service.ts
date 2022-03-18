18cfd8388454a574e4dfa1a354ca637367144771
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-03-17 21:55:25 -0300

    get game details on service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -12,15 +12,42 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  getGameList(ordering: string, search?: string): Observable<APIResponse<Game>> {
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
     let params = new HttpParams().set('ordering', ordering);
 
     if (search) {
       params = new HttpParams().set('ordering', ordering).set('search', search);
     }
 
-    return this.http.<APIResponse<Game>>(`${env.BASE_URL}/games`, {
-      params: params
-    })
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params,
+    });
+  }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
   }
 }
